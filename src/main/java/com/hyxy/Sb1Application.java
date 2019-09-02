7d1be7173647ec2f9413728fb6f4817872a29322
dzc <dzc.@163.com>
2019-09-02 18:51:19 +0800

    dzc

--- src/main/java/com/hyxy/Sb1Application.java
+++ src/main/java/com/hyxy/Sb1Application.java
@@ -4,8 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
-@SpringBootApplication
+ 
+@SpringBootApplication 
 @ComponentScan(basePackages = {"com.hyxy.*"})
 @MapperScan("com.hyxy.dao")
 public class Sb1Application {  
